db9d8cf09e7a5dad74aabb5ef19f79818f9891e3
laramontana <larabrightside@gmail.com>
2017-10-24 16:45:07 +0300

    Done.

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,7 +1,7 @@
 module SongsHelper
 
-  def artist_select(song)
-    if song.artist
+  def artist_select(song, artist)
+    if artist
       text_field :song, :artist_name
     else
       select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
